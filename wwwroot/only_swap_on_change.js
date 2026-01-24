44e0288670ca77909a6895ccad402257380169ba
gitgoggles <ruckojr@hotmail.com>
2026-01-24 01:46:26 +0000

    fixed chrome issue with js file

--- wwwroot/only_swap_on_change.js
+++ wwwroot/only_swap_on_change.js
@@ -1,6 +1,6 @@
 document.addEventListener("htmx:beforeSwap", (e) => {
   const incoming = (e.detail.serverResponse || "").trim();
-  const current = (e.originalTarget.innerHTML || "").trim();
+  const current = (e.target.innerHTML || "").trim();
 
   if (incoming === current) {
     e.detail.shouldSwap = false;
